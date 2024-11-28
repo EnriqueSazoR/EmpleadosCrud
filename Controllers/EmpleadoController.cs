71c9f78c1d270ab99053c061e4e3148cc7101cb4
EnriqueSazoR <luisrosales9598@gmail.com>
2024-11-27 20:05:32 -0600

    Se agrega metodo insert

--- Controllers/EmpleadoController.cs
+++ Controllers/EmpleadoController.cs
@@ -22,5 +22,20 @@ namespace MvcEmpleados.Controllers
             List<Empleado> lista = await _appDbContext.Empleados.ToListAsync();
             return View(lista);
         }
+
+        [HttpGet]
+        public IActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Nuevo(Empleado empleado)
+        {
+            await _appDbContext.Empleados.AddAsync(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+
     }
 }
